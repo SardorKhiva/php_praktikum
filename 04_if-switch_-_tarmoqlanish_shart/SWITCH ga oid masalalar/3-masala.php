71035209faeb5076ab01dfb64b229169ec0cd3b2
Leader <leaderkhiva@gmail.com>
2024-12-10 08:55:28 +0500

    switch ga oid 3-masala

--- 04_if-switch_-_tarmoqlanish_shart/SWITCH ga oid masalalar/3-masala.php
+++ 04_if-switch_-_tarmoqlanish_shart/SWITCH ga oid masalalar/3-masala.php
@@ -1,6 +1,15 @@
 <?php
 /*
  * 3-masala.
- * Berilgan son(1 dan 999 999  gacha bo`lgan sonlar)ni
+ * Berilgan son (1 dan 999 999 gacha bo`lgan sonlar) ni
  * so`zlarda ifodalang.
  */
+
+$son = 0;
+
+do {
+echo "1-1_000_000 oraliqdagi sonni kiiriting: ";
+$son = intval(trim(readline()));
+} while($son > 0 && $son < 1_000_000);
+
+echo $son;
