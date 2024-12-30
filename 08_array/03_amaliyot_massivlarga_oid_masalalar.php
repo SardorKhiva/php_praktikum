101c01d40ca9f71edaedbf929f9094a74926b0e5
Leader <leaderkhiva@gmail.com>
2024-12-30 19:26:27 +0500

    amaliyot_massivlarga_oid_masalalar demo

--- 08_array/03_amaliyot_massivlarga_oid_masalalar.php
+++ 08_array/03_amaliyot_massivlarga_oid_masalalar.php
@@ -29,7 +29,7 @@ for ($i = 0; $i < count($sonlar2); $i++) {
 echo "3 ga karrali indexli elementlar summasi = ", $sum . PHP_EOL;
 */
 
-
+/*
 // eng katta elementli indexni topish
 $natijalar = [
     "Abdulla" => 12,
@@ -53,3 +53,60 @@ foreach ($natijalar as $key => $value)
 
 echo "Katta element = ", $katta_element . PHP_EOL;
 echo "Katta element indexi = ", $katta_element_index . PHP_EOL;
+*/
+
+// MASSIVLARNI SARALASH
+
+// bo'sh massiv
+$element = [];
+
+// massivni har xil sonlar bilan 10 ta elementga to'ldirish
+for ($i = 0; $i < 10; $i++) {
+    $element[] = rand(1, 100);
+//    arr_print($element);
+}
+
+echo "Saralanmagan massiv: \n";
+arr_print($element);
+//print_r($element);      // chiqarish
+echo PHP_EOL;
+
+$j = 0;
+
+echo "Saralangan massiv: \n";
+for ($i = 0; $i < count($element); $i++) {
+    arr_print($element);    // har odimdagi o'zgarishlarni chiqarish
+
+    // 0-dan boshlab kichik deb elementni saqlab turish
+    $kichik = $element[$i];
+
+    // i dan keyingi (saralanganlardan boshqa)larni siklga kiritish
+    for ($j = $i + 1; $j < count($element); $j++) {
+
+        // agar i dan o'ngda turgan element chapdagidan kichik bo'lsa
+        if ($element[$j] < $kichik) {       // kamayish tartibida saralash uchun > yoziladi
+            $kichik = $element[$j];     // kichik element shu bo'lsin
+
+            // o'rin almashtirish
+//            $temp = $element[$j];           // o'ngdagini vaqtincha saqlab turish
+            $element[$j] = $element[$i];    // o'ngdagi chapdagini qiymatini saqlasin
+//            $element[$i] = $temp;           // chapdagi esa vaqtincha saqlab turilganni o'zida saqlasin
+            $element[$i] = $kichik;         // temp bo'lmasa
+
+            // almashgan ikki son
+            echo " { $element[$j] } > { $element[$i] } " . PHP_EOL;
+        }
+    }
+//    echo $element[$i] . " < " . $element[$j] . PHP_EOL;
+    echo PHP_EOL;
+}
+
+//print_r($element);
+
+function arr_print($massiv): void
+{
+    for ($i = 0; $i < count($massiv); $i++) {
+        echo $massiv[$i] . " ";
+    }
+    echo PHP_EOL;
+}
