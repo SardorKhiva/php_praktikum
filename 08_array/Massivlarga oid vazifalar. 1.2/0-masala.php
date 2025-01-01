59d593ab756d721b9d8232999d779763922f3aa9
root <root@localhost>
2025-01-01 20:30:32 +0500

    Massivlarga oid vazifalar. 1.2 jildi qoʻshildi

---
+++ 08_array/Massivlarga oid vazifalar. 1.2/0-masala.php
@@ -0,0 +1,6 @@
+<?php
+// 0-masala. $n = array(12, 5, 2, 66, 22, 56, 123, 7, 1, 67).
+// Ushbu massivni elementlari o’sish tartibda saralang va hosil bo’lgan yangi
+// massivni ekranga chiqaring.
+// Natija : 1, 2, 5, 7, 12, 22, 56, 66, 67, 123
+?>
