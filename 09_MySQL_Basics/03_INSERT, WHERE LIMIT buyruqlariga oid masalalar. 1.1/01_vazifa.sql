e9313159055cdeaadb4e4390643b3f0dcd78604d
Leader <leaderkhiva@gmail.com>
2025-01-18 13:53:30 +0500

    4 ta vazifa ishlandi

--- 09_MySQL_Basics/03_INSERT, WHERE LIMIT buyruqlariga oid masalalar. 1.1/01_vazifa.sql
+++ 09_MySQL_Basics/03_INSERT, WHERE LIMIT buyruqlariga oid masalalar. 1.1/01_vazifa.sql
@@ -1,4 +1,33 @@
 # 1) O`zingizda mavjud biror jadvalga :
 # - bitta ma`lumot qo`shing
 # - 3 ta ma`lumot kiriting
-# - faqat boshlang`ich qiymatlari yo`q bo`lgan ustunlarga qiymat berib kiriting
+# - faqat boshlang`ich qiymatlari yo`q
+# bo`lgan ustunlarga qiymat berib kiriting
+
+SHOW DATABASES;
+USE `Mashq`;
+SHOW TABLES;
+CREATE TABLE `darslar_ruyhati`
+(
+    `id`        INT AUTO_INCREMENT PRIMARY KEY,
+    `dars_nomi` VARCHAR(30) NOT NULL
+);
+
+SHOW TABLES;
+SELECT * FROM `darslar_ruyhati`;
+
+INSERT INTO `darslar_ruyhati` (`dars_nomi`)
+VALUES
+    ("Ona tili"),
+    ("Matematika"),
+    ("Odobnoma"),
+    ("Jismoniy tarbiya"),
+    ("Tasviriy san'at"),
+    ("Rus tili"),
+    ("Ingliz tili"),
+    ("Mantiq"),
+    ("Huquqshunslik asoslari"),
+    ("Tarix"),
+    ("Islom asoslari");
+
+SELECT * FROM `darslar_ruyhati`;
--- 09_MySQL_Basics/03_INSERT, WHERE LIMIT buyruqlariga oid masalalar. 1.1/04_vazifa.sql
+++ 09_MySQL_Basics/03_INSERT, WHERE LIMIT buyruqlariga oid masalalar. 1.1/04_vazifa.sql
@@ -1,3 +1,30 @@
-# 4)"employees" jadvalidan:
-# - "Van" familyali erkaklar orasidan, emp_no qiymati 10000 va 30000 ni orasida yotganlarni chiqaring
-# - "Van" familyali ayollar orasida, 1962- yilda tug`ilganlarni chiqaring
+# 4-masala:
+# "employees" jadvalidan:
+# - "Van" familyali erkaklar orasidan,
+# emp_no qiymati 10000 va 30000 ni
+# orasida yotganlarni chiqaring
+SHOW DATABASES;
+USE `Mashq`;
+SHOW TABLES;
+
+SELECT * FROM `employees` LIMIT 10;
+# SELECT COUNT(*) FROM `employees`;   -- 300 024 ta satr
+
+SELECT *            -- hamma ustunlar tanlab
+FROM `employees`    -- employees jadvalidan
+WHERE `emp_no`      -- qaysiki emp_no ustunidagi
+ BETWEEN 10000 AND 30000 -- 10000 va 30000 qiymatlilari
+ AND `first_name` = 'Van' -- va familyasi Van bo'lgan
+ AND `gender` = 'M';      -- va jinsi erkak bo'lganlar ko'rsatilsin
+
+# - "Van" familyali ayollar orasidan,
+# 1962- yilda tug`ilganlarni chiqaring
+SELECT *
+FROM `employees`
+WHERE `last_name` LIKE 'Van'
+AND `gender` = 'F'
+AND YEAR(`birth_date`) = 1962;
+
+/*
+emp_no  birth_date  first_name  last_name   gender  hire_date
+*/
