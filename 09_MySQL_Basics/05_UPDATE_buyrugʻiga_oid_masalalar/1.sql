0b6651bb77b42f121e85a099c9459c0c7e727e37
Leader <leaderkhiva@gmail.com>
2025-01-20 12:02:39 +0500

    08_UPDATE_REPLACE1

--- 09_MySQL_Basics/05_UPDATE_buyrugʻiga_oid_masalalar/1.sql
+++ 09_MySQL_Basics/05_UPDATE_buyrugʻiga_oid_masalalar/1.sql
@@ -1,6 +1,11 @@
 # 1)"employees" jadvalida:
 # - "Bondur" ismli employeelarni
 # officeCode qiymatlari 2 ga o`zgartiring
+USE `Mashq`;
+SHOW DATABASES;
+SHOW TABLES;
+SELECT * FROM `employees` LIMIT 10;
+
 
 # - "spatterson@classicmodelcars.com"
 # emailli employeeni ismini "Jim" ga,
