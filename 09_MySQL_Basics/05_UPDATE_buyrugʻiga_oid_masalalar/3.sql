5d9e2f60a3c4a62106b97dd6229eee3e5dbfa8e2
Leader <leaderkhiva@gmail.com>
2025-01-21 08:42:18 +0500

    3-masala to'liq ishlandi

--- 09_MySQL_Basics/05_UPDATE_buyrugʻiga_oid_masalalar/3.sql
+++ 09_MySQL_Basics/05_UPDATE_buyrugʻiga_oid_masalalar/3.sql
@@ -1,7 +1,7 @@
 /* 3)"products" jadvalidan:
 skladdagi soni 3000 dan kam
 bo`lganlarni barchasini sotiladigan
-narxini 15 % ga kamaytiring */
+narxini 15 % ga kamaytiring */      -- SOTILISH NARHI QOLIB SOTIB OLISH NARHINI O'ZGARTIRIBMAN
 SHOW DATABASES;
 USE `Mashq`;
 SHOW TABLES;
@@ -106,13 +106,175 @@ WHERE `quantityInStock` < 3000;
 
 /* "Motorcycles" turidagi maxsulotlarni
 barchasini sotilish narxini 15% ga oshiring */
+SELECT `productName` AS "Mahsulot nomi",
+       `MSRP` AS "Sotilish narhi"
+FROM `products`
+WHERE `productLine` = 'Motorcycles';
+/* natija:
++---------------------------------------+----------------+
+| Mahsulot nomi                         | Sotilish narhi |
++---------------------------------------+----------------+
+| 1969 Harley Davidson Ultimate Chopper |         126.57 |
+| 1996 Moto Guzzi 1100i                 |         157.30 |
+| 2003 Harley-Davidson Eagle Drag Bike  |         256.12 |
+| 2002 Suzuki XREO                      |         199.19 |
+| 1936 Harley Davidson El Knucklehead   |          80.11 |
+| 1957 Vespa GS150                      |          82.23 |
+| 1997 BMW R 1100 S                     |         149.05 |
+| 1960 BSA Gold Star DBD34              |          89.16 |
+| 1982 Ducati 900 Monster               |          91.60 |
+| 1997 BMW F650 ST                      |         116.91 |
+| 1982 Ducati 996 R                     |          53.20 |
+| 1974 Ducati 350 Mk3 Desmo             |         134.96 |
+| 2002 Yamaha YZR M1                    |          95.23 |
++---------------------------------------+----------------+
+13 rows in set (0.00 sec)
+ */
 
+UPDATE `products`
+SET `MSRP` = `MSRP` * (100 - 15) / 100
+WHERE `productLine` = "Motorcycles";
+/*
+ Query OK, 13 rows affected, 11 warnings (0.05 sec)
+Rows matched: 13  Changed: 13  Warnings: 11
+ */
+SELECT `productName` AS "Mahsulot nomi",
+       `MSRP`        AS "Sotilish narhi"
+FROM `products`
+WHERE `productLine` = "Motorcycles";
+/* natija:
++---------------------------------------+----------------+
+| Mahsulot nomi                         | Sotilish narhi |
++---------------------------------------+----------------+
+| 1969 Harley Davidson Ultimate Chopper |          91.44 |
+| 1996 Moto Guzzi 1100i                 |         113.65 |
+| 2003 Harley-Davidson Eagle Drag Bike  |         185.05 |
+| 2002 Suzuki XREO                      |         143.91 |
+| 1936 Harley Davidson El Knucklehead   |          57.88 |
+| 1957 Vespa GS150                      |          59.42 |
+| 1997 BMW R 1100 S                     |         107.69 |
+| 1960 BSA Gold Star DBD34              |          64.42 |
+| 1982 Ducati 900 Monster               |          66.18 |
+| 1997 BMW F650 ST                      |          84.46 |
+| 1982 Ducati 996 R                     |          38.44 |
+| 1974 Ducati 350 Mk3 Desmo             |          97.51 |
+| 2002 Yamaha YZR M1                    |          68.81 |
++---------------------------------------+----------------+
+13 rows in set (0.00 sec)   */
 
 
 /* "Classic Cars" turidagi skladdagi
 soni 1000 dan kam bo`lgan maxlusotlarni,
 sotib olingan narxini 10% oshirib,
 sotilishi narxini 18% oshiring */
+SELECT `productName` AS "\Mahsulot nomi",
+       `quantityInStock` AS "\Mahsulotlar soni",
+       `buyPrice` AS "\Sotib olish narhi",
+       `MSRP` AS "\Sotish narhi"
+FROM `products`
+WHERE `productLine` = "Classic Cars";
+/* natija:
++-------------------------------------+------------------+-------------------+--------------+
+| Mahsulot nomi                       | Mahsulotlar soni | Sotib olish narhi | Sotish narhi |
++-------------------------------------+------------------+-------------------+--------------+
+| 1952 Alpine Renault 1300            |             7305 |             98.58 |       214.30 |
+| 1972 Alfa Romeo GTA                 |             3252 |             85.68 |       136.00 |
+| 1962 LanciaA Delta 16V              |             6791 |            103.42 |       147.74 |
+| 1968 Ford Mustang                   |               68 |             98.06 |       203.20 |
+| 2001 Ferrari Enzo                   |             3619 |             95.59 |       207.80 |
+| 1969 Corvair Monza                  |             6906 |             89.14 |       151.08 |
+| 1968 Dodge Charger                  |             9123 |             75.16 |       117.44 |
+| 1969 Ford Falcon                    |             1049 |             70.59 |       129.77 |
+| 1970 Plymouth Hemi Cuda             |             5663 |             31.92 |        79.80 |
+| 1969 Dodge Charger                  |             7323 |             58.73 |       115.16 |
+| 1993 Mazda RX-7                     |             3975 |             83.51 |       141.54 |
+| 1965 Aston Martin DB5               |             9042 |             65.96 |       124.44 |
+| 1948 Porsche 356-A Roadster         |             8826 |             53.90 |        77.00 |
+| 1995 Honda Civic                    |             9772 |             93.89 |       142.25 |
+| 1998 Chrysler Plymouth Prowler      |             4724 |            101.51 |       163.73 |
+| 1999 Indy 500 Monte Carlo SS        |             8164 |             56.76 |       132.00 |
+| 1992 Ferrari 360 Spider red         |             8347 |             77.90 |       169.34 |
+| 1985 Toyota Supra                   |             7733 |             57.01 |       107.57 |
+| 1969 Dodge Super Bee                |             1917 |             41.69 |        60.31 |
+| 1976 Ford Gran Torino               |             9127 |             73.49 |       146.99 |
+| 1948 Porsche Type 356 Roadster      |             8990 |             62.16 |       141.28 |
+| 1970 Triumph Spitfire               |             5545 |             91.92 |       143.62 |
+| 1957 Corvette Convertible           |             1249 |             59.44 |       111.60 |
+| 1957 Ford Thunderbird               |             3209 |             34.21 |        71.27 |
+| 1970 Chevy Chevelle SS 454          |             1005 |             41.85 |        55.12 |
+| 1970 Dodge Coronet                  |             4074 |             32.37 |        57.80 |
+| 1966 Shelby Cobra 427 S/C           |             8197 |             29.18 |        50.31 |
+| 1949 Jaguar XK 120                  |             2350 |             40.16 |        68.15 |
+| 1958 Chevy Corvette Limited Edition |             2542 |             13.52 |        26.52 |
+| 1952 Citroen-15CV                   |             1452 |             61.90 |        88.08 |
+| 1982 Lamborghini Diablo             |             7723 |             16.24 |        37.76 |
+| 1969 Chevrolet Camaro Z28           |             4695 |             50.51 |        85.61 |
+| 1971 Alpine Renault 1600s           |             7995 |             38.58 |        61.23 |
+| 2002 Chevy Corvette                 |             9446 |             62.11 |       107.08 |
+| 1956 Porsche 356A Coupe             |             6600 |             98.30 |       140.43 |
+| 1992 Porsche Cayenne Turbo Silver   |             6582 |             69.78 |       118.28 |
+| 1961 Chevrolet Impala               |             7869 |             32.33 |        80.84 |
+| 1982 Camaro Z28                     |             6934 |             46.53 |       101.15 |
++-------------------------------------+------------------+-------------------+--------------+
+38 rows in set (0.01 sec)  */
+
+UPDATE `products`
+SET    `buyPrice` = `buyPrice` + (`buyPrice` / 10),
+       `MSRP` = `MSRP` * (100 - 18) / 100
+WHERE `productLine` = "Classic Cars";
+/* Query OK, 38 rows affected, 69 warnings (0.02 sec)
+Rows matched: 38  Changed: 38  Warnings: 69     */
+
+SELECT `productName` AS "\Mahsulot nomi",
+       `quantityInStock` AS "\Mahsulotlar soni",
+       `buyPrice` AS "\Sotib olish narhi",
+       `MSRP` AS "\Sotish narhi"
+FROM `products`
+WHERE `productLine` = "Classic Cars";
+/* natija:
++-------------------------------------+------------------+-------------------+--------------+
+| Mahsulot nomi                       | Mahsulotlar soni | Sotib olish narhi | Sotish narhi |
++-------------------------------------+------------------+-------------------+--------------+
+| 1952 Alpine Renault 1300            |             7305 |            108.44 |       175.73 |
+| 1972 Alfa Romeo GTA                 |             3252 |             94.25 |       111.52 |
+| 1962 LanciaA Delta 16V              |             6791 |            113.76 |       121.15 |
+| 1968 Ford Mustang                   |               68 |            107.87 |       166.62 |
+| 2001 Ferrari Enzo                   |             3619 |            105.15 |       170.40 |
+| 1969 Corvair Monza                  |             6906 |             98.05 |       123.89 |
+| 1968 Dodge Charger                  |             9123 |             82.68 |        96.30 |
+| 1969 Ford Falcon                    |             1049 |             77.65 |       106.41 |
+| 1970 Plymouth Hemi Cuda             |             5663 |             35.11 |        65.44 |
+| 1969 Dodge Charger                  |             7323 |             64.60 |        94.43 |
+| 1993 Mazda RX-7                     |             3975 |             91.86 |       116.06 |
+| 1965 Aston Martin DB5               |             9042 |             72.56 |       102.04 |
+| 1948 Porsche 356-A Roadster         |             8826 |             59.29 |        63.14 |
+| 1995 Honda Civic                    |             9772 |            103.28 |       116.65 |
+| 1998 Chrysler Plymouth Prowler      |             4724 |            111.66 |       134.26 |
+| 1999 Indy 500 Monte Carlo SS        |             8164 |             62.44 |       108.24 |
+| 1992 Ferrari 360 Spider red         |             8347 |             85.69 |       138.86 |
+| 1985 Toyota Supra                   |             7733 |             62.71 |        88.21 |
+| 1969 Dodge Super Bee                |             1917 |             45.86 |        49.45 |
+| 1976 Ford Gran Torino               |             9127 |             80.84 |       120.53 |
+| 1948 Porsche Type 356 Roadster      |             8990 |             68.38 |       115.85 |
+| 1970 Triumph Spitfire               |             5545 |            101.11 |       117.77 |
+| 1957 Corvette Convertible           |             1249 |             65.38 |        91.51 |
+| 1957 Ford Thunderbird               |             3209 |             37.63 |        58.44 |
+| 1970 Chevy Chevelle SS 454          |             1005 |             46.04 |        45.20 |
+| 1970 Dodge Coronet                  |             4074 |             35.61 |        47.40 |
+| 1966 Shelby Cobra 427 S/C           |             8197 |             32.10 |        41.25 |
+| 1949 Jaguar XK 120                  |             2350 |             44.18 |        55.88 |
+| 1958 Chevy Corvette Limited Edition |             2542 |             14.87 |        21.75 |
+| 1952 Citroen-15CV                   |             1452 |             68.09 |        72.23 |
+| 1982 Lamborghini Diablo             |             7723 |             17.86 |        30.96 |
+| 1969 Chevrolet Camaro Z28           |             4695 |             55.56 |        70.20 |
+| 1971 Alpine Renault 1600s           |             7995 |             42.44 |        50.21 |
+| 2002 Chevy Corvette                 |             9446 |             68.32 |        87.81 |
+| 1956 Porsche 356A Coupe             |             6600 |            108.13 |       115.15 |
+| 1992 Porsche Cayenne Turbo Silver   |             6582 |             76.76 |        96.99 |
+| 1961 Chevrolet Impala               |             7869 |             35.56 |        66.29 |
+| 1982 Camaro Z28                     |             6934 |             51.18 |        82.94 |
++-------------------------------------+------------------+-------------------+--------------+
+38 rows in set (0.01 sec)   */
 
 /* MSRP - sotilish narxi
 buyPrice - sotib olingan    */
