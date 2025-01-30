646d4e46b874241bf52e563dbb3e0ad39e58aaa6
Leader <leaderkhiva@gmail.com>
2025-01-30 20:56:15 +0500

    JOINʼlar boʻyicha vazifalar. 1.2 8-va 9--masalalar berilishi

---
+++ 10_MySQL_Advanced/17.15_JOINʼlarBoʻyichaVazifalar.1.2/10.sql
@@ -0,0 +1,20 @@
+/* 10-masala:
+Mijozlar qaysi hodimning mijozi
+ekanligi haqida ma`lumot chiqaring.
+Mijozning ismi, familyasi,
+hodimning ismi, familyasi.
+Mijozning ismi bo`yicha saralang */
+/*---------------+---------------------+----------------+---------------------+
+| mijozning_ismi | mijozning_familyasi | hodimning_ismi | hodimning_familyasi |
++----------------+---------------------+----------------+---------------------+
+| Adrian         | Huxley              | Andy           | Fixter              |
+| Akiko          | Shimamura           | Mami           | Nishi               |
+| Alejandra      | Camino              | NULL           | NULL                |
+| Alexander      | Feuer               | NULL           | NULL                |
+| Alexander      | Semenov             | NULL           | NULL                |
+| Allen          | Nelson              | Julie          | Firrelli            |
+| Ann            | Brown               | Larry          | Bott                |
+| Anna           | O`Hara              | Andy           | Fixter              |
+| Annette        | Roulet              | Gerard         | Hernandez           |
+| Armand         | Kuger               | NULL           | NULL                |
+| Arnold         | Cruz                | Mami           | Nishi               | */
---
+++ 10_MySQL_Advanced/17.15_JOINʼlarBoʻyichaVazifalar.1.2/9.sql
@@ -0,0 +1,18 @@
+/* 9-masala:
+Har bir mijoz, har yilda qancha
+$ lik to`lov qilganini aniqlang.
+Familya bo`yicha saralang.
++------------------+-----------------+------+-----------+
+| contactFirstName | contactLastName | yil  | jami      |
++------------------+-----------------+------+-----------+
+| Adrian           | Huxley          | 2003 |  24013.52 |
+| Adrian           | Huxley          | 2005 |  31835.36 |
+| Adrian           | Huxley          | 2004 |  35806.73 |
+| Akiko            | Shimamura       | 2004 |  70965.55 |
+| Akiko            | Shimamura       | 2005 |  34583.18 |
+| Allen            | Nelson          | 2003 |  32680.31 |
+| Allen            | Nelson          | 2005 |  28322.83 |
+| Allen            | Nelson          | 2004 |  12530.51 |
+| Ann              | Brown           | 2003 |  66884.91 |
+| Ann              | Brown           | 2004 |  13671.82 |
+| Anna             | O'Hara          | 2005 |  56932.30 | */
