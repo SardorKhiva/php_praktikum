a52aa9ac74dd7c8f65929e4d3a2d6cebe63e0161
Leader <leaderkhiva@gmail.com>
2025-02-01 18:45:42 +0500

    UNION boʻyicha vazifalar. 1-va 2-masalalar berilishi

---
+++ 10_MySQL_Advanced/17.18_UNION_boʻyicha_vazifalar/1.sql
@@ -0,0 +1,13 @@
+/*  UNION boʻyicha vazifalar
+1-masala
+Barcha mijozlar va hodimlarning ismi, familyasini va mijoz bo'lsa mijoz deb, hodim bo`lsa hodim deb chiqsin.
++------------------+-----------------+-------+
+| ismi             | familyasi       | mijoz |
++------------------+-----------------+-------+
+| Valarie          | Franco          | mijoz |
+| Tony             | Snowden         | mijoz |
+| Diane            | Murphy          | hodim |
+| Mary             | Patterson       | hodim |
+| Jeff             | Firrelli        | hodim |
+| William          | Patterson       | hodim |
+| Gerard           | Bondur          | hodim | */
---
+++ 10_MySQL_Advanced/17.18_UNION_boʻyicha_vazifalar/2.sql
@@ -0,0 +1,24 @@
+/* 2-masala.
+Fransiyada ishlaydigan ishchilarni va Fransiyalik mijozlarni ismi va familyasini  chiqaring
++------------------+-----------------+---------+
+| contactFirstName | contactLastName | country |
++------------------+-----------------+---------+
+| Carine           | Schmitt         | France  |
+| Janine           | Labrune         | France  |
+| Mary             | Saveley         | France  |
+| Martine          | Rancé           | France  |
+| Marie            | Bertrand        | France  |
+| Frédérique       | Citeaux         | France  |
+| Annette          | Roulet          | France  |
+| Daniel           | Da Silva        | France  |
+| Daniel           | Tonini          | France  |
+| Laurence         | Lebihan         | France  |
+| Paul             | Henriot         | France  |
+| Dominique        | Perrier         | France  |
+| Gerard           | Bondur          | France  |
+| Loui             | Bondur          | France  |
+| Gerard           | Hernandez       | France  |
+| Pamela           | Castillo        | France  |
+| Martin           | Gerard          | France  |
++------------------+-----------------+---------+
+17 rows in set (0.23 sec) */
