1417340d52fc8f2f440722b556a0e17cc05fbee0
Leader <leaderkhiva@gmail.com>
2025-07-28 16:11:55 +0500

    08_array/08_Array_functions.5-qism.php

--- 08_array/08_Array_functions.5-qism.php
+++ 08_array/08_Array_functions.5-qism.php
@@ -1,150 +1,50 @@
 <?php
 
-// array_intersect() - 1-massivdagi elementlarni
-// 2-massivdagi elementlar bilan solishtiradi va
-// 1-massivda mavjud bo'lgan,
-// lekin keyingi massivlarda ham mavjud bo'lgan elementlarni qaytaradi.
+// list - massiv elementlari qiymatlarini list parametridagi o'zgaruvchilarga beradi
 /*
-$massiv1 = [
-    "a" => "green", "b" => "red", "c" => "blue"
+$web_massiv = [
+    'html', 'css', 'js', 'php'      // shu elementlarni pastdagi list funksiya parametridagi o'zgaruvchilarga beradi
 ];
-$massiv2 = [
-    "d" => "yellow", "e" => "purple", "f" => "orange",
-    "a" => "green", "b" => "red"    // index farqli bo'lsa ham, qiymatlari bir xil bo'lgan elementlar olinadi
-];
-$natija = array_intersect($massiv1, $massiv2);
-echo "Massiv 1: \n";
-print_r($massiv1);
-echo "Massiv 2: \n";
-print_r($massiv2);
-echo "1-va 2-massivda ham mavjud bo'lgan elementlar: \n";
-// massivdagi elementlarni solishtirib, 1-massivda mavjud bo'lgan,
-// lekin 2-massivda ham mavjud bo'lgan elementlarni qaytaradi
-print_r($natija);
+
+list($HTML, $CSS, $JS, $PHP) = $web_massiv; // massiv elementlarini o'zgaruvchilarga beradi
+echo "Web dasturlash tillari: \n";
+echo "HTML: $HTML\n";
+echo "CSS: $CSS\n";
+echo "JS: $JS\n";
+echo "PHP: $PHP\n";
 */
 
-// array_unique() - massivdagi takroriy elementlarni olib tashlaydi
-// va yangi massiv qaytaradi.
+// range() - berilgan qiymatlar oralig'idagi massiv hosil qiladi
 /*
-$takroriy_massiv = [
-    "a" => "green", "b" => "red", "c" =>
-        "blue", "d" => "green", "e" => "red"
-];
-echo "Takroriy massiv: \n";
-print_r($takroriy_massiv);
-$unikal_massiv = array_unique($takroriy_massiv);
-echo "Takroriy elementlar olib tashlangan massiv: \n";
-print_r($unikal_massiv); // massivdagi takroriy elementlarni olib tashlaydi
+$range_demo = range(1, 10); // 1 dan 10 gacha bo'lgan sonlar massivini hosil qiladi
+echo "1 dan 10 gacha bo'lgan sonlar: \n";
+print_r($range_demo); // 1 dan 10 gacha bo'lgan sonlar massivini chiqaradi
+
+$range_demo_2 = range(1, 10, 2); // 1 dan 10 gacha 2 qadam bilan sonlar massivini hosil qiladi
+print_r($range_demo_2);
 */
 
-// extract() - massivdagi string kalitlarni shu nomli o'zgaruvchilarga
-// aylantiradi va qiymatlarini o'zgaruvchilarga beradi.
+// shuffle(&array) - massiv elementlarini (kalitlarinimas) aralashtiradi - asosiy massivni o'zgartiradi
 /*
-$massiv = [
-    "a" => "green", "b" => "red", "c" => "blue"
-];
-echo "Massiv: \n";
+$range_demo = range(1, 15); // 1 dan 10 gacha bo'lgan sonlar massivini hosil qiladi
+echo "Aralashmagan massiv:\n";
+print_r($range_demo);
+
+// Aralashmagan massivni aralashtirish
+shuffle($range_demo);
+echo "Aralashgan massiv:\n";
+print_r($range_demo);
+*/
+
+// sort(&array) - massivni o'sib borish tartibida saralaydi - asosiy massivni o'zgartiradi
+$massiv = range(1, 15); // 1 dan 15 gacha bo'lgan sonlar massivini hosil qiladi
+echo "Original massiv:\n";
 print_r($massiv);
-extract($massiv); // massivdagi string kalitlarni o'zgaruvchilarga aylantiradi
-echo "O'zgaruvchilar: \n";
-echo "a = $a\n"; // $a o'zgaruvchisi massivdagi "a" kalitining qiymatini oldi
-echo "b = $b\n"; // $b o'zgaruvchisi massivdagi "b" kalitining qiymatini oldi
-echo "c = $c\n"; // $c o'zgaruvchisi massivdagi "c" kalitining qiymatini oldi
-*/
 
-// compact() - o'zgaruvchilarni massivga aylantiradi.
-// O'zgaruvchilar nomlarini massiv kalitlari, qiymatlarini esa qiymatlarini oladi.
-/*
-$yashil = "green";
-$qizil = "red";
-$qora = "black";
-$ranglar = compact("yashil", "qizil", 'qora');
-echo "O'zgaruvchilardan hosil bo'lgan massiv: \n";
-print_r($ranglar); // o'zgaruvchilar nomlarini massiv kalitlari,
-*/
+echo "\nAralashgan massiv:\n";
+shuffle($massiv); // massivni aralashtirish
+print_r($massiv);
 
-// array_values() - massivdagi qiymatlarni oladi
-// va yangi indeksli massiv qaytaradi.
-/*
-$dasturlash = [
-    "PHP" => "PHP",
-    "JavaScript" => "JavaScript",
-    "Python" => "Python",
-    7 => "Java"
-];
-echo "Dasturlash tillari: \n";
-print_r($dasturlash);
-echo "Index li yangi massiv: \n";
-print_r(array_values($dasturlash));
-*/
-
-// array_keys() - massivdagi kalitlarni oladi
-// va yangi indeksli massiv qaytaradi.
-/*
-// 1-holat:
-$dasturlash = [
-    "P" => "PHP",
-    "JS" => "JavaScript",
-    "Py" => "Python",
-    7 => "Java",
-    9 => "PHP",
-    'son' => 8,
-    'satr' => '8'
-];
-echo "Dasturlash tillari: \n";
-print_r($dasturlash);
-echo "Kalitlar: \n";
-// 1-holat:
-// massivdagi kalitlarni oladi va
-// yangi indeksli massiv qaytaradi
-print_r(array_keys($dasturlash));
-
-// 2-holat:
-// massivdagi ma'lum qiymatlarni oladi va
-// shu qiymat indexlaridan yangi indeksli massiv qaytaradi
-$tanlangan_element = array_keys($dasturlash, 'PHP'); // massivdagi qiymatlarni oladi va yangi indeksli massiv qaytaradi
-print_r($tanlangan_element);
-
-// 3-holat:
-// massivdagi ma'lum qiymatlarni oladi va
-// shu qiymat indexlaridan yangi
-// indeksli massiv qaytaradi,
-// tiplarini ham tekshiradi
-$tanlangan_elementlar = array_keys($dasturlash, 'PHP', true);
-// massivdagi qiymatlarni oladi va
-// yangi indeksli massiv qaytaradi,
-// tiplarini ham tekshiradi
-*/
-
-// array_key_exists() - massivda ma'lum kalit mavjudligini tekshiradi
-/*
-$massiv = [
-    "P" => "PHP",
-    "JS" => "JavaScript",
-    "Py" => "Python",
-    7 => "Java",
-    9 => "PHP",
-    'son' => 8,
-    'satr' => '8'
-];
-
-echo array_key_exists('P', $massiv) ?
-    "Kalit 'P' mavjud\n" : "Kalit 'P' mavjud emas\n"; // true)
-*/
-
-// array_rand() - massivdan tasodifiy elementlarni oladi
-/*
-$massiv = [
-    'talaba1' => 'Ali',
-    'talaba2' => 'Dinora',
-    'talaba3' => 'Mahmud'
-];
-
-$element = array_rand($massiv);
-echo "Tasodifiy element: " . $massiv[$element] . "\n"; // tasodifiy elementni oladi
-
-// 2 ta element kalitini random tanlash
-$elementlar = array_rand($massiv, 2);
-print_r($elementlar);
-*/
-
+echo "\nSaralangan massiv:\n";
+sort($massiv); // massivni o'sib borish tartibida saralash
+print_r($massiv);
