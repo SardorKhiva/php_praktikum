80958627885ff1f5e748e1ca52ba83595fda2c36
Leader <leaderkhiva@gmail.com>
2025-10-28 11:20:03 +0500

    foreign key takrorlash

--- 10_MySQL_Advanced/05_17.9_Foreign_key.1-qism.sql
+++ 10_MySQL_Advanced/05_17.9_Foreign_key.1-qism.sql
@@ -8,40 +8,25 @@ USE `mashq`;
 -- 1-usul:
 CREATE TABLE IF NOT EXISTS `student`
 (
-    `id`        INT UNSIGNED NOT NULL AUTO_INCREMENT,
+    `id`        INT UNSIGNED PRIMARY KEY AUTO_INCREMENT,
     `name`      VARCHAR(30)  NOT NULL,
     `course_id` INT DEFAULT NULL,
-    PRIMARY KEY (`id`),
     FOREIGN KEY (`course_id`)    -- shu jadvaldagi shu nomli ustun
-        REFERENCES course (`id`) -- course jadvalidagi id ustuniga bog'lansin
+        REFERENCES `course` (`id`) -- course jadvalidagi id ustuniga bog'lansin
 );
 
 # 2-usul: FROEIGN KEY index ga nom berish
 CREATE TABLE IF NOT EXISTS `student_2`
 (
-    `id`        INT UNSIGNED NOT NULL AUTO_INCREMENT,
-    `name`      VARCHAR(30)  NOT NULL,
-    `course_id` INT DEFAULT NULL FOREIGN KEY REFERENCES course(`id`),
-    PRIMARY KEY (`id`),
-    CONSTRAINT `FK_student_c` FOREIGN KEY (course_id)
-        REFERENCES course (`id`)
-);
-/*
-Tepada keltirilgan kodda kichik sintaksis xatosi bor.
-FOREIGN KEYga nom berishni alohida ko'rsatganingiz uchun,
-course_id ustuni ustida bir vaqtning o'zida FOREIGN KEY
-e'lon qilinishi kerak emas. Buning o'rniga, CONSTRAINT
-orqali alohida e'lon qilinadi.
- */
-CREATE TABLE IF NOT EXISTS `student_2`
-(
-    `id`        INT UNSIGNED NOT NULL AUTO_INCREMENT,
+    `id`        INT UNSIGNED PRIMARY KEY AUTO_INCREMENT,
     `name`      VARCHAR(30)  NOT NULL,
     `course_id` INT DEFAULT NULL,
-    PRIMARY KEY (`id`),
-    CONSTRAINT `FK_student_c` FOREIGN KEY (`course_id`)
+    CONSTRAINT `FK_student_c`
+        FOREIGN KEY (`course_id`)
         REFERENCES `course` (`id`)
 );
+
+
 /*
 Tushuntirish:
 
@@ -55,13 +40,13 @@ e'tibor berish tavsiya etiladi (masalan, FK_student_c cheklov nomi "student" va 
 
 #       FOREIGN KEY i yo'q bo'lgan jadvaldagi ustunga FOREIGN KEY qo'shish:
 ALTER TABLE `student`
-    ADD FOREIGN KEY (`course_id`) REFERENCES course (`id`);
+    ADD FOREIGN KEY (`course_id`) REFERENCES `course` (`id`);
 
 -- index ga nomni  o'zimiz xohlagan nom berib qo'shish:
 ALTER TABLE `student`
     ADD CONSTRAINT `FK_student_c`
         FOREIGN KEY (`course_id`)
-            REFERENCES course (`id`);
+            REFERENCES `course` (`id`);
 
 #       FOREIGN KEY ni o'chirish:
 ALTER TABLE `student`
