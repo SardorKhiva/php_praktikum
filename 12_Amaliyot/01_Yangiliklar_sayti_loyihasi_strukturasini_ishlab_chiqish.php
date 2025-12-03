816ca80976a1683ef80c172ffc1c0b631806efa0
Leader <leaderkhiva@gmail.com>
2025-12-03 09:28:40 +0500

    toza php da yangiliklar amaliyot sayti

---
+++ 12_Amaliyot/01_Yangiliklar_sayti_loyihasi_strukturasini_ishlab_chiqish.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * Foydalanuvchi: User
+ * Loyiha nomi: PRAKTIKUM
+ * Fayl nomi: 01_Yangiliklar_sayti_loyihasi_strukturasini_ishlab_chiqish.php
+ * Fayl yaratilgan: 02.12.2025 10:29
+ * Maqsad:
+ */
+
---
+++ 12_Amaliyot/02_Loyihaning_Database_strukturasini_ishlab_chiqish.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * Foydalanuvchi: User
+ * Loyiha nomi: PRAKTIKUM
+ * Fayl nomi: 02_Loyihaning_Database_strukturasini_ishlab_chiqish.php
+ * Fayl yaratilgan: 02.12.2025 10:43
+ * Maqsad:
+ */
+
