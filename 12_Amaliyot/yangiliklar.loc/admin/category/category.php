4bc551f7420318c43671630c4d7297d734b307d5
LeaderWin <leaderkhiva@gmail.com>
2025-12-30 23:30:19 +0500

    gitignore ga 12_Amaliyot/yangiliklar.loc/.osp qo'shildi

---
+++ 12_Amaliyot/yangiliklar.loc/admin/category/category.php
@@ -0,0 +1,77 @@
+<?php
+require_once __DIR__ . '/../header.php';
+require_once __DIR__ . "/category_helper.php";     // mysql bazaga ulanish
+
+$page = $_GET['page'] ?? 1;
+
+$limit = $_POST['viewItemCount'] ?? $_GET['limit'] ?? 10;
+
+?>
+    <div class="container">
+        <div class="row">
+            <h1>Kategoriyalar ro'yhati</h1>
+        </div>
+        <div class="mt-3 mb-3">
+            <a href="../category/add_category.php" class="btn btn-success">Qo'shish</a>
+        </div>
+        <table class="table table-dark table-hover table-striped">
+            <thead>
+            <tr>
+                <th scope="col"> ID</th>
+                <th scope="col"> Nomi:</th>
+                <th scope="col"> Amallar:</th>
+            </tr>
+            </thead>
+            <tbody>
+            <?php foreach (getCategoryList($page, $limit) as $kategoriya): ?>
+                <tr>
+                    <td> <?= $kategoriya['id'] ?> </td>
+                    <td> <?= $kategoriya['title'] ?> </td>
+                    <td>
+                        <a href="../category/edit_category.php?id=<?= $kategoriya['id']; ?>"
+                           class="btn btn-primary">Tahrirlash</a>
+                        <a href="../category/delete_category.php?id=<?= $kategoriya['id']; ?>"
+                           class="btn btn-danger">O'chirish</a>
+                    </td>
+                </tr>
+            <?php endforeach; ?>
+            </tbody>
+        </table>
+
+        <!--   Paginatsiya qismi     -->
+        <nav aria-label="Page navigation example">
+            <ul class="pagination justify-content-center">
+                <?php for ($sahifa = 1; $sahifa <= getPaginationCategory($limit, 'category'); $sahifa++): ?>
+                    <li class="page-item">
+                        <a class="page-link <?php if ($sahifa == $_GET['page']) echo 'active'; ?>"
+                           href="../category/category.php?page= <?= $sahifa; ?>&limit=<?= $limit; ?>"> <?= $sahifa; ?>
+                        </a>
+                    </li>
+                <?php endfor; ?>
+            </ul>
+        </nav>
+
+
+        <form method="post" class="form-label pb-3">
+            <select class="form-select" aria-label="Default select example" name="viewItemCount">
+                <option selected>Nechtadan ma'lumot chiqsin</option>
+                <?php for ($sahifa = 1;
+                           $sahifa <= getCategoryTotalRows();
+                           $sahifa++): ?>
+                    <option value="<?= $sahifa; ?>"
+                            <?php if ($sahifa == $limit): ?>selected<?php endif; ?>>
+                        <?= $sahifa ?>
+                    </option>
+                <?php endfor; ?>
+            </select>
+            <input type="submit"
+                   name="inputItemCount"
+                   value="Ko'rsatish"
+                   class="btn btn-success mt-3">
+        </form>
+
+    </div>
+
+
+<?php
+require_once __DIR__ . '/../footer.php';
---
+++ 12_Amaliyot/yangiliklar.loc/admin/category/delete_category.php
@@ -0,0 +1,32 @@
+<?php
+require_once __DIR__ . '/../header.php';
+require_once __DIR__ . '/category_helper.php';
+
+
+if (isset($_GET['id'])) {
+    $id = (int)($_GET['id']) ?? 0;
+    $category_row = getCategoryById($id);
+    $title = $category_row['title'];
+
+    if ($_GET['confirm'] === 'yes') {
+        deleteCategory($id);
+    }
+}
+?>
+    <div class="container">
+        <label for="cat_del_no" class="mt-3 mb-3">Rostdan ham <strong> <?= $title ?></strong> kategoriyasini
+            o'chirmoqchimisiz?
+            <br>
+            <a href="../category/delete_category.php?id=<?= $id ?>&confirm=yes"
+               id="cat_del_yes"
+               class="btn btn-danger">Ha
+            </a>
+            <a href="../category/category.php"
+               id="cat_del_no"
+               class="btn btn-primary">Yo'q
+            </a>
+        </label>
+    </div>
+
+<?php
+require_once __DIR__ . '/../footer.php';
